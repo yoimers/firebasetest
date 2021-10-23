b9a80de3b2ffcec553e53fb9194a35edfc24ddc2
yoimers <yomehalf@gmail.com>
2021-10-23 14:52:52 +0900

    first

---
+++ src/app.controller.ts
@@ -0,0 +1,18 @@
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { AppService } from './app.service';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
+  }
+
+  @Post()
+  postHello(@Body() body) {
+    console.log(body);
+    return 'aa';
+  }
+}
